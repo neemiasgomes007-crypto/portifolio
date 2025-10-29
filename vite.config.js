b2c7f42356c62c1175ac2bb21f37593a947dd02e
neemiasgomes007 <neemiasgomes007@gmail.com>
2025-10-28 23:58:13 -0300

    Add Vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,17 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    }
   }
 })
